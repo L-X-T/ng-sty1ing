82dcfe20a6ea799697c48d93926b346cf25ca9c1
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2024-04-17 11:42:43 +0200

    06 Grid

--- src/app/lab-06-modern/lab-06-modern.component.ts
+++ src/app/lab-06-modern/lab-06-modern.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-lab-06-modern',
@@ -7,4 +7,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './lab-06-modern.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Lab06ModernComponent {}
+export class Lab06ModernComponent {
+  protected readonly expanded = signal(false);
+
+  protected onToggleSide(): void {
+    this.expanded.update((expanded) => !expanded);
+  }
+}
