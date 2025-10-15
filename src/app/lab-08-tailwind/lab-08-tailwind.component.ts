cfd85c78cc8cd167b4500aa45e086fc42393ac0d
Alexander Thalhammer <alexander.thalhammer@gmail.com>
2025-10-15 15:07:08 +0200

    08 Tailwind

--- src/app/lab-08-tailwind/lab-08-tailwind.component.ts
+++ src/app/lab-08-tailwind/lab-08-tailwind.component.ts
@@ -8,4 +8,6 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './lab-08-tailwind.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Lab08TailwindComponent {}
+export class Lab08TailwindComponent {
+  flexboxDivs = Array.from({ length: 8 }, (_, i) => i + 1);
+}
